8b2926ec80fa065fd5fd2f241a9cd1d34c2d1c97
Marcelo Gonzalez <mdgonzalez470@gmail.com>
2023-05-29 13:38:48 +0300

    segundaEntregav1

--- 17_SegundaEntrega/src/index.js
+++ 17_SegundaEntrega/src/index.js
@@ -26,53 +26,7 @@ const server = app.listen(process.env.PORT, () => {
     console.log("Server on port", process.env.PORT)
 })
 
-// // Función para generar un código de producto único
-// function generateProductCode() {
-//     var code = '';
-//     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-//     for (var i = 0; i < 6; i++) {
-//       code += characters.charAt(Math.floor(Math.random() * characters.length));
-//     }
-//     return code;
-//   }
-  
-//   // Lista de categorías y productos
-//   var categories = {
-//     lacteos: ['Leche', 'Queso', 'Yogurt'],
-//     fiambres: ['Jamon', 'Mortadela'],
-//     carniceria: ['Lomo', 'Cerdo', 'Queperi', 'Costilla', 'Chorizo', 'Morcilla'],
-//     panaderia: ['Pan', 'Facturas', 'Chipa', 'Churros'],
-//     verduras: ['Tomate', 'Cebolla', 'Pimiento', 'Zapallo', 'Papa'],
-//     frutas: ['Naranja', 'Mandarina', 'Manzana', 'Pera', 'Uva', 'Cereza'],
-//     bebidas: ['Agua', 'Refresco', 'Jugo']
-//   };
-  
-//   // Generar una lista de productos aleatorios
-//   var products = [];
-//   var productId = 1;
-//   for (var category in categories) {
-//     if (categories.hasOwnProperty(category)) {
-//       var productList = categories[category];
-//       for (var i = 0; i < productList.length; i++) {
-//         var product = {
-//           title: productList[i],
-//           description: 'Descripción de ' + productList[i],
-//           code: generateProductCode(),
-//           category: category,
-//           price: Math.random() * 100,
-//           stock: Math.floor(Math.random() * 100),
-//           status: true,
-//           thumbnail: []
-//         };
-//         products.push(product);
-//         productId++;
-//       }
-//     }
-//   }
-  
-  // Insertar los productos en la colección 'productos'
-//   await productModel.insertMany(products);
-// await productModel.deleteMany({});
+
 //ServerIO Sockets
 
 const io = new Server(server)
